6b1b1ff6c5836db196df68c9f505478c086cb13c
Hamza Siddiq <92462002+hamza-siddiq@users.noreply.github.com>
2024-05-29 14:51:30 -0700

    Initial Commit CardView isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hamza on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
